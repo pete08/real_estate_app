c635e538f13fbf5200f74e8aa9c65ee8eb398167
PeterH <pete08@gmail.com>
2020-07-08 11:24:34 -0500

    Add show route file API request
    Add seed_dump gem

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -8,14 +8,15 @@ class Api::ListingsController < ApplicationController
   def create
     @listing = Listing.new(
       description: params[:description], 
-      year_built:"1880", #Start here, build out user input to create Segment Parameters for listing a house. Add a function to convert address to longitude and latitude variables
-      square_feet:1250,
-      bedrooms:5, 
-      bathrooms:5, 
-      availability:true, 
-      price:400_000, 
-      address:"3450 Original st., middle, nowhere",latitude:8234453, 
-      longitude:563456
+      year_built: params[:year_built],
+      square_feet: params[:square_feet],
+      bedrooms: params[:bedrooms], 
+      bathrooms: params[:bathrooms], 
+      availability: params[:availability], 
+      price: params[:price], 
+      address:params[:address],
+      latitude: params[:latitude], 
+      longitude: params[:longitude]
     )
     @listing.save
     render "show.json.jb"
